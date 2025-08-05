8bf2df62e89f9e7dc2c92aaed53050d60269b6db
Adrianof1 <adrianoferreira8@outlook.com>
2025-08-04 23:40:53 -0300

    adicionando função if , alert e return.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 
+//Captura o campo input e o nome que o usúario digita.
 function adicionarAmigo() {
     let amigoInput = document.getElementById('amigo');
     let nomeAmigo = amigoInput.value;
 
-    )
+//faz a validação da entrada e retorna o alerta se não houver nome válido.
+    if (nomeAmigo.trim() !== '') {
+        alert('Por Favor, digite um nome válido.')
+        return;//para a execução
+    }
