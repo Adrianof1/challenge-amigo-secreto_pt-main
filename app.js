8343a311faafd9bf6536b61372ece0e5d2555062
Adrianof1 <adrianoferreira8@outlook.com>
2025-08-04 23:28:29 -0300

    adicionando let amigos e função adicionar amigos

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = []
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigoInput = document.getElementById('amigo');
+    let nomeAmigo = amigoInput.value;
+
+    )
