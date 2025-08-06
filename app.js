cdba78baf5d4d53bbcea47231915a61e3aa54ae6
Adrianof1 <adrianoferreira8@outlook.com>
2025-08-05 21:00:09 -0300

    adionando funtion adicionarAmigos

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 let amigos = []
 
+function adicionarAmigo() {
+    let amigoInput = document.getElementById('amigo'); //pega o elemento pelo seu id.
+    let nomeAmigo = amigoInput.value; //pega o texto que o usúario digitou dentro do campo input.
+    if (nomeAmigo.trim() === '') {
+        alert('Por favor, digite um nome válido.'); //verifica se o campo está vazio ou contém apenas espaços.
+        return; //se estiver vazio, não adiciona o amigo e sai da função.
+    }
+
+    if (amigos.includes(nomeAmigo)) {
+        alert('Este nome já foi adicionado. Por favor, insira um nome diferente.')
+        amigoInput.value = ''; //limpa o campo.
+        return; //para a execução.
+    }
+
+    amigos.push(nomeAmigo); //se passou em todas as validações, adiciona o nome ao array 'amigos'.
+}
